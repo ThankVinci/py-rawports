a40be6ca1647070925561befecb74dd9b76483be
ThankVinci <thankvinci@163.com>
2025-12-27 00:58:10 +0800

    fix pack

--- setup.py
+++ setup.py
@@ -1,5 +1,17 @@
 from setuptools import setup
+from setuptools.command.bdist_wheel import bdist_wheel
 import os
 
+class BDistWheelWithTag(bdist_wheel):
+    def get_tag(self):
+        python = "py38"
+        abi = "none"
+        plat = "any" # self.plat_name
+        return python, abi, plat
+
 version = os.getenv('PKG_VERSION', '0.0.1a0')
-setup(version=version)
+setup(version=version, 
+      cmdclass={
+        'bdist_wheel': BDistWheelWithTag,
+        }
+    )
