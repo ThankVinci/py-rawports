4b44fba6ae2c1b8bc0024f91788908587ee93b9f
ThankVinci <thankvinci@163.com>
2025-12-27 23:00:17 +0800

    upgrade prebuild.sh

--- scripts/shell/prebuild.sh
+++ scripts/shell/prebuild.sh
@@ -4,6 +4,10 @@ if [ -e "./build/bdist.macosx-15.0-arm64" ]; then
     rm -rf "./build/bdist.macosx-15.0-arm64"
 fi
 
+if [ -e "./build/bdist.linux-x86_64" ]; then
+    rm -rf "./build/bdist.linux-x86_64"
+fi
+
 if [ -e "./build/lib" ]; then
     rm -rf "./build/lib"
 fi
